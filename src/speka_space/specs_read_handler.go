764d63a0bea2be3e8b0733289ed5bc758daa3884
Egor Chistiakov <e.chistiakov@city-mobil.ru>
2021-12-06 16:30:35 +0300

    save state

--- src/speka_space/specs_read_handler.go
+++ src/speka_space/specs_read_handler.go
@@ -1,21 +1,35 @@
 package speka_space
 
 import (
-	"encoding/json"
 	gen "git.redmadrobot.com/internship/backend/lim-ext/src/generated"
 	"git.redmadrobot.com/internship/backend/lim-ext/src/pkg/openapi"
+	"git.redmadrobot.com/internship/backend/lim-ext/src/repository/ent/specification"
 	"net/http"
 )
 
 func (s Server) RetrieveSpecifications(w http.ResponseWriter, r *http.Request, params gen.RetrieveSpecificationsParams) {
 	s.logger.Debug().Msg("Handle RetrieveSpecifications request")
-	var req gen.RetrieveSpecificationsParams
-	err := json.NewDecoder(r.Body).Decode(&req)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+	author := params.Author
+	if author != nil {
+		specs, err := s.repo.Client.Specification.Query().
+			Where(specification.AuthorEQ(*author)).All(r.Context())
+		if err != nil {
+		}
+		openapi.Resp(w, http.StatusOK, specs)
 		return
 	}
+	//status := params.Status
+	//if status != nil {
+	//	specs, err := s.repo.Client.Specification.Query().
+	//		Where(specification.StatusEQ(*status)).All(r.Context())
+	//	if err != nil {
+	//	}
+	//	openapi.Resp(w, http.StatusOK, specs)
+	//	return
+	//}
 
 	specs, err := s.repo.Client.Specification.Query().All(r.Context())
+	if err != nil {
+	}
 	openapi.Resp(w, http.StatusOK, specs)
 }
